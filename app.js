271213ed3428d5c1db66435da3ac7f37d400d50d
Phan Ngoc Thanh Binh <binhphan2509@gmail.com>
2021-07-01 22:25:20 +0700

    initial

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const morgan = require('morgan');
 const config =require ('./config/config.json');
-
-
-
 const app = express();
+
+app.use(express.json());
+app.use(morgan('dev'));
+
+app.use('/user',require('./routes/user.route'));
 
 const PORT = config.PORT;
 app.listen(PORT, function () {
