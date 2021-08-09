8cec074c0f3fc4075384af46005b5e607b338b09
PhuocChanhTran <46354066+PhuocChanhTran@users.noreply.github.com>
2021-08-10 03:21:23 +0700

    Update db.js

--- utils/db.js
+++ utils/db.js
@@ -3,7 +3,7 @@ const knex = require("knex")({
   connection: {
     host: "127.0.0.1",
     user: "root",
-    password: "123456",
+    password: "admin",
     database: "online_courses",
   },
   pool: { min: 0, max: 100 },
