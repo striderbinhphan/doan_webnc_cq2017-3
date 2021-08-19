4d941b0245204398cda4fda82b5d0f408741254f
PhuocChanhTran <phuocchanh1998@gmail.com>
2021-08-20 02:14:47 +0700

    Disable user, edit category,disable list, login page

--- utils/db.js
+++ utils/db.js
@@ -3,7 +3,7 @@ const knex = require("knex")({
   connection: {
     host: "127.0.0.1",
     user: "root",
-    password: "password",
+    password: "123456",
     database: "online_courses",
   },
   pool: { min: 0, max: 100 },
